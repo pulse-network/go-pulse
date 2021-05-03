44bc70ecb51742882df0c06d9b95686c8eece3db
Shane Bammel <sjb933@gmail.com>
2021-05-03 18:34:31 -0500

    Fix initPulsors.

--- consensus/parlia/primordialpulse_fork.go
+++ consensus/parlia/primordialpulse_fork.go
@@ -14,12 +14,12 @@ import (
 // authorization snapshot on a new PulseChain fork.
 func (p *Parlia) initPulsors() ([]common.Address, error) {
 	// get validators from parlia config
-	if p.config.InitValidators == nil || len(p.config.InitValidators) == 0 {
+	if p.config.InitValidators == nil || len(*p.config.InitValidators) == 0 {
 		return nil, errors.New("missing initValidators in parlia config")
 	}
 
-	validators := make([]common.Address, len(p.config.InitValidators))
-	for i, addr := range p.config.InitValidators {
+	validators := make([]common.Address, len(*p.config.InitValidators))
+	for i, addr := range *p.config.InitValidators {
 		validators[i] = common.HexToAddress(addr)
 	}
 	return validators, nil
