9829eb9235c9f867650c6aba3d6834524ac1b719
Bret Palsson <bretep@gmail.com>
2021-11-02 12:36:13 -0700

    Update foundation comment

--- mobile/params.go
+++ mobile/params.go
@@ -78,7 +78,7 @@ func GoerliGenesis() string {
 }
 
 // FoundationBootnodes returns the enode URLs of the P2P bootstrap nodes operated
-// by the pulsechain core team running the V5 discovery protocol.
+// by the foundation running the V5 discovery protocol.
 func FoundationBootnodes() *Enodes {
 	nodes := &Enodes{nodes: make([]*enode.Node, len(params.MainnetBootnodes))}
 	for i, url := range params.MainnetBootnodes {
