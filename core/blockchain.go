8c6a10133b5a3bc11d2e50dee81fc5d0cc3637b9
Shane Bammel <sjb933@gmail.com>
2021-12-15 22:29:44 -0600

    Record bad block signer

--- core/blockchain.go
+++ core/blockchain.go
@@ -2378,11 +2378,12 @@ Chain config: %v
 
 Number: %v
 Hash: 0x%x
+Coinbase: 0x%x
 %v
 
 Error: %v
 ##############################
-`, bc.chainConfig, block.Number(), block.Hash(), receiptString, err))
+`, bc.chainConfig, block.Number(), block.Hash(), block.Coinbase(), receiptString, err))
 }
 
 // InsertHeaderChain attempts to insert the given header chain in to the local
