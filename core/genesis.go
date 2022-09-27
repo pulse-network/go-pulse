b79693e66d2c577dfc66334f3bb6de93cfaca8f9
Shane Bammel <sjb933@gmail.com>
2022-09-27 12:38:06 -0500

    Add flags for pulsechain and pulsechain-testnet

--- core/genesis.go
+++ core/genesis.go
@@ -454,6 +454,18 @@ func DefaultGenesisBlock() *Genesis {
 	}
 }
 
+// DefaultPulseChainGenesisBlock returns the PulseChain mainnet genesis block.
+func DefaultPulseChainGenesisBlock() *Genesis {
+	return &Genesis{
+		Config:     params.PulseChainConfig,
+		Nonce:      66,
+		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
+		GasLimit:   5000,
+		Difficulty: big.NewInt(17179869184),
+		Alloc:      decodePrealloc(mainnetAllocData),
+	}
+}
+
 // DefaultRopstenGenesisBlock returns the Ropsten network genesis block.
 func DefaultRopstenGenesisBlock() *Genesis {
 	return &Genesis{
@@ -511,6 +523,18 @@ func DefaultKilnGenesisBlock() *Genesis {
 		panic(err)
 	}
 	return g
+}
+
+// DefaultPulseChainTestnetGenesisBlock returns the PulseChain testnet genesis block.
+func DefaultPulseChainTestnetGenesisBlock() *Genesis {
+	return &Genesis{
+		Config:     params.PulseChainTestnetConfig,
+		Nonce:      66,
+		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
+		GasLimit:   5000,
+		Difficulty: big.NewInt(17179869184),
+		Alloc:      decodePrealloc(mainnetAllocData),
+	}
 }
 
 // DeveloperGenesisBlock returns the 'geth --dev' genesis block.
--- params/bootnodes.go
+++ params/bootnodes.go
@@ -32,6 +32,14 @@ var MainnetBootnodes = []string{
 	"enode://4aeb4ab6c14b23e2c4cfdce879c04b0748a20d8e9b59e25ded2a08143e265c6c25936e74cbc8e641e3312ca288673d91f2f93f8e277de3cfa444ecdaaf982052@157.90.35.166:30303",   // bootnode-hetzner-fsn
 }
 
+// PulseChainBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main PulseChain network.
+var PulseChainBootnodes []string // TODO
+
+// PulseChainTestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main PulseChain network.
+var PulseChainTestnetBootnodes []string // TODO
+
 // RopstenBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Ropsten test network.
 var RopstenBootnodes = []string{
