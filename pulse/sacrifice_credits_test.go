12969b78eb3cc31d3ca375a2e4806097b7dd862f
Shane Bammel <sjb933@gmail.com>
2023-02-26 20:28:00 -0600

    Update credits file
    Includes updated sacrifice data & eth2 deposit refunds

--- pulse/sacrifice_credits_test.go
+++ pulse/sacrifice_credits_test.go
@@ -37,8 +37,8 @@ func TestApplySacrificeCredits(t *testing.T) {
 	}
 
 	// from the credits.csv file in compressed-allocations
-	actual = state.GetBalance(common.HexToAddress("0x0000000000000000000000000000000000001010"))
-	expected, _ = new(big.Int).SetString("5977597164464952199640526", 10)
+	actual = state.GetBalance(common.HexToAddress("0x000000005dCEE11e13fb536Fa40d65450F53c5a8"))
+	expected, _ = new(big.Int).SetString("64000000000000000000", 10)
 
 	if actual.Cmp(expected) != 0 {
 		t.Errorf("Invalid sacrifice credit balance, actual: %d, expected: %d", actual, expected)
