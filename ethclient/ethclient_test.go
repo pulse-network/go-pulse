63fde364a1f4fd18092333ff4bb2e9a9dcf5e92a
Shane Bammel <sjb933@gmail.com>
2023-03-24 22:53:42 -0500

    Add padding to gas estimations
    Adds a 20% buffer to gas estimations to reduce out-of-gas errors.

--- ethclient/ethclient_test.go
+++ ethclient/ethclient_test.go
@@ -542,7 +542,7 @@ func testCallContractAtHash(t *testing.T, client *rpc.Client) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if gas != 21000 {
+	if gas != 25200 {
 		t.Fatalf("unexpected gas price: %v", gas)
 	}
 	block, err := ec.HeaderByNumber(context.Background(), big.NewInt(1))
@@ -569,7 +569,7 @@ func testCallContract(t *testing.T, client *rpc.Client) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if gas != 21000 {
+	if gas != 25200 {
 		t.Fatalf("unexpected gas price: %v", gas)
 	}
 	// CallContract
